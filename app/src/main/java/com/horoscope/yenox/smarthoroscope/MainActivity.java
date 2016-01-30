acc1e304a57dd09723d4b343d968982c1a6ac46d
yen0x <gege.dert@gmail.com>
2016-01-30 18:08:10 +0100

    init swipe activity

--- app/src/main/java/com/horoscope/yenox/smarthoroscope/MainActivity.java
+++ app/src/main/java/com/horoscope/yenox/smarthoroscope/MainActivity.java
@@ -1,13 +1,27 @@
 package com.horoscope.yenox.smarthoroscope;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.horoscope.yenox.smarthoroscope.fragments.SignListFragment;
+import com.horoscope.yenox.smarthoroscope.fragments.sign.SignContent;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity//
+        implements SignListFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignContent.addSignItems(Arrays.asList(getResources().getStringArray(R.array.sign_names)));
+    }
+
+    @Override
+    public void onListFragmentInteraction(SignContent.SignItem item) {
+        Intent intent = new Intent(this, HoroscopeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/horoscope/yenox/smarthoroscope/fragments/sign/SignContent.java
@@ -0,0 +1,40 @@
+package com.horoscope.yenox.smarthoroscope.fragments.sign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p/>
+ */
+public class SignContent {
+
+    public static final List<SignItem> SIGN_ITEMS = new ArrayList<SignItem>();
+
+    static {
+    }
+
+    public static void addSignItems(List<String> list) {
+        for (String sign : list) {
+            addItem(new SignItem(sign));
+        }
+    }
+
+    private static void addItem(SignItem item) {
+        SIGN_ITEMS.add(item);
+    }
+
+    public static class SignItem {
+        public final String name;
+
+        public SignItem(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
